6eedcd7f19940642cfbcaa8384670e79a3a893b9
Garu <teledavid7@gmail.com>
2025-09-19 14:14:08 +0200

    Se añadió una línea inservible

--- src/com/ercilla/PersonaRegistro.java
+++ src/com/ercilla/PersonaRegistro.java
@@ -2,7 +2,7 @@ package com.ercilla;
 
 public record PersonaRegistro(String nombre, String apellidos) {
     public String toString(){
-        System.out.println("Esta instrucción es inservible")
+        System.out.println("Esta instrucción es inservible");
         return "%s %s" .formatted(nombre, apellidos);
     }
 }
