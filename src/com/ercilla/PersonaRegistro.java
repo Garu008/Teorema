95a5c33c88e629ad09b263ef958fdf61ed2e86be
Garu008 <teledavid7@gmail.com>
2025-09-23 12:01:48 +0200

    Código inservible añadido

--- src/com/ercilla/PersonaRegistro.java
+++ src/com/ercilla/PersonaRegistro.java
@@ -2,7 +2,7 @@ package com.ercilla;
 
 public record PersonaRegistro(String nombre, String apellidos) {
     public String toString(){
-        //Jai 
+        //Pepe ha escrito el código inservible
         //System.out.println("Esta instrucción es inservible");
         return "%s %s" .formatted(nombre, apellidos);
     }
