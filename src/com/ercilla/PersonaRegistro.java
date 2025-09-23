a672d2af52e5939697c07a7341d7a3e87264d808
Garu008 <teledavid7@gmail.com>
2025-09-23 11:54:27 +0200

    Comentario sobre código inservible

--- src/com/ercilla/PersonaRegistro.java
+++ src/com/ercilla/PersonaRegistro.java
@@ -3,7 +3,7 @@ package com.ercilla;
 public record PersonaRegistro(String nombre, String apellidos) {
     public String toString(){
         //Jai 
-        System.out.println("Esta instrucción es inservible");
+        //System.out.println("Esta instrucción es inservible");
         return "%s %s" .formatted(nombre, apellidos);
     }
 }
